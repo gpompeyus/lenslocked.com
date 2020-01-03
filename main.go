5afdca7813e2172290090b765732ea9806b15c81
gpompeyus <german.pompeyus@gmail.com>
2020-01-03 19:14:01 -0400

    Se aumentaron nuevas cosas con el nuevo router

--- main.go
+++ main.go
@@ -13,14 +13,26 @@ func home (w http.ResponseWriter, r *http.Request) {
 }
 
 func contact (w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type","text/html")
-		fmt.Fprint(w, "Para mayor informacion.. bla bla")
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Para mayor informacion.. bla bla")
+}
 
+func faq (w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Coloca aqui tus dudas y te responderemos lo antes posible")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "No pudimos encontrar la pagina buscada")
 }
 
 func main() {
 	r:=mux.NewRouter()
+	r.NotFoundHandler=http.HandlerFunc(notFound)
 	r.HandleFunc("/",home)
 	r.HandleFunc("/contact",contact)
+	r.HandleFunc("/faq",faq)
 	http.ListenAndServe(":3000", r)
 }

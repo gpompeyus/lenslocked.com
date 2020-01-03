badda1a79f4d8689e78afb8eb99f1d85b561e9da
gpompeyus <german.pompeyus@gmail.com>
2020-01-03 17:26:31 -0400

    Se agrego el status 404 not found

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type","text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "Para mayor informacion.. bla bla")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w,"No podemos encontrar la pagina que buscas")
+	}
 }
 
 func main() {
